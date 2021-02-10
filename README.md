a8d439b3052162db79247e22c0303b6416de7b72
Patrick Cowland <patrickcowland@pingidentity.com>
2021-02-10 15:08:19 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ASP.NET Core Samples
+This repository contains code samples that demonstrate how to use Ping SignIn and SignOut in ASP.NET Core projects.
+
+## Samples:
+- [**OpenID Connect sample**](pingone-customers-sample-oidc) - User SignIn and SignOut using OIDC
---
+++ pingone-netcore-sdk/PingOne.Core/Configuration/PingOneConfigurationBase.cs
@@ -0,0 +1,11 @@
+﻿namespace PingOne.Core.Configuration
+{
+    public abstract class PingOneConfigurationBase
+    {
+        public string AuthBaseUrl { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string Secret { get; set; }
+    }
+}
